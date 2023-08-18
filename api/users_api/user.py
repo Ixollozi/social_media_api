75d3e80f86889c87bfe47749c78283441b9dafa2
Your Name <you@example.com>
2023-08-18 17:00:47 +0500

    aa

--- api/users_api/user.py
+++ api/users_api/user.py
@@ -1,11 +1,22 @@
 from fastapi import Request
 from pydantic import BaseModel
 from typing import List, Dict
-from database.user_service import register_user_db, check_user_data_db, check_user_password_db, change_user_data, profile_info
+from database.user_service import register_user_db, check_user_data_db,\
+    check_user_password_db, change_user_data, profile_info
 from api import app
+import re
 
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
+
+def mail_cheker(email):
+    if re.fullmatch(regex, email):
+        return True
+    return False
 
 # модель пользователя
+
+
 class User(BaseModel):
     name: str
     mail: str
@@ -14,12 +25,47 @@ class User(BaseModel):
     user_city: str
 
 # регистрация пользователя
+
+
 @app.post('/api/registeration')
 async def register_user(user_model: User):
     user_data = dict(user_model)
-    try:
-        reg_user = register_user_db(**user_data)
-    except Exception as e:
-        print(e)
+    mail_validation = mail_cheker(user_model.mail)
+    if mail_validation:
+        try:
+            reg_user = register_user_db(**user_data)
+            return {'status': 1, 'user_id': reg_user}
+        except Exception as e:
+            return {'status': 0, 'message': e}
+    return {'status': 0, 'message': "invalid email"}
 
-    return {'status': 1}
+# получить данные пользорвателя по  user_id
+
+
+@app.get('/api/user')
+async def get_user(user_id: int):
+    exact_user = profile_info(user_id)
+    return {'status': 1, 'user_id': exact_user}
+
+# вход в аккаунт
+
+
+@app.get('/api/login')
+async def login_user(mail: str, password: str):
+    # валидация переданной почтой
+    mail_validation = mail_cheker(mail)
+    if mail_validation:
+        login_checker = str(check_user_password_db(mail, password))
+        # если все данные верны
+        if login_checker.isdigit():
+            return {'status': 1, 'user_id': int(login_checker)}
+        return {'status': 0, 'message': login_checker}
+    return {'status': 0, 'message': 'Invalid email'}
+
+# изменение  данных о пользователе
+
+
+@app.put('/api/change_profile')
+async def change_user_profile(user_id: int, change_nfo: str, new_data: str):
+    data = change_user_data(user_id, new_data, change_nfo)
+    return {'status': 1, 'message': data}
--- database/user_service.py
+++ database/user_service.py
@@ -9,7 +9,7 @@ def register_user_db(name, mail, phone_number, password, user_city):
     db = next(get_db())
     new_user = User(name=name, mail=mail, phone_number=phone_number,
                     password=password, user_city=user_city, reg_date=datetime.now())
-    #добавляем в базу
+    # добавляем в базу
     db.add(new_user)
     # сохраним в базу
     db.commit()
@@ -20,27 +20,29 @@ def register_user_db(name, mail, phone_number, password, user_city):
 
 def check_user_data_db(phone_number, mail):
     db = next(get_db())
-    #проверка данных на наличие записи в базе
+    # проверка данных на наличие записи в базе
     checker = db.query(User).filter_by(phone_number=phone_number, mail=mail).first()
-    #если есть совпадение
+    # если есть совпадение
     if checker:
         return False
-    #если нет совпадения
+    # если нет совпадения
     return True
 
 # проверка пользователя при входе в аккаунт
+
+
 def check_user_password_db(mail, password):
     db = next(get_db())
-    #проверка данных на наличие записи в базе
+    # проверка данных на наличие записи в базе
     checker = db.query(User).filter_by(mail=mail).first()
-    #ессли нашел такйо эмеил, проверяем правильность пароля
+    # ессли нашел такйо эмеил, проверяем правильность пароля
     if checker:
-        #начинаем сверку пароля
+        # начинаем сверку пароля
         if checker.password == password:
             return checker.id
         else:
             return 'неверный пароль'
-    #если не находит в базе
+    # если не находит в базе
     return 'неверная почта'
 
 # получить информации о пользователе
@@ -48,20 +50,21 @@ def check_user_password_db(mail, password):
 
 def profile_info(user_id):
     db = next(get_db())
-    #првоерка пользователя через айди
+    # првоерка пользователя через айди
     exact_user = db.query(User).filter_by(id=user_id).first()
-    #если нашел пользователя, передаю всю пользователю
+    # если нашел пользователя, передаю всю пользователю
     if exact_user:
         return exact_user.mail, exact_user.phone_number,\
-            exact_user.id, exact_user.name, exact_user.reg_date, exact_user.city
-    #если не нашел пользователя
+            exact_user.id, exact_user.name, exact_user.reg_date, exact_user.user_city
+    # если не нашел пользователя
     return 'нет такого пользователя'
+
 
 def change_user_data(user_id, change_info, new_data):
     db = next(get_db())
-    #находим пользователя в базе
+    # находим пользователя в базе
     exact_user = db.query(User).filter_by(id=user_id).first()
-    #проверка того, какую информацию хочет изменить пользователь
+    # проверка того, какую информацию хочет изменить пользователь
     if exact_user:
         if change_info == 'email':
             exact_user.mail = new_data
@@ -75,7 +78,7 @@ def change_user_data(user_id, change_info, new_data):
             exact_user.password = new_data
         db.commit()
         return 'данные успешно изменены'
-    #а если не находим в базе пользователя
+    # а если не находим в базе пользователя
     return 'пользователь не найден'
 
 # сброс пароля
@@ -83,9 +86,9 @@ def change_user_data(user_id, change_info, new_data):
 
 def delete_password(user_id, password):
     db = next(get_db())
-    #находим пользователя в базе
+    # находим пользователя в базе
     exact_user = db.query(User).filter_by(id=user_id).first()
-    #проверка того, какую информацию хочет изменить пользователь
+    # проверка того, какую информацию хочет изменить пользователь
     if exact_user:
         if exact_user.password == password:
             exact_user.password = 'password'
@@ -94,5 +97,5 @@ def delete_password(user_id, password):
             return 'пароль успешно сброшен'
         else:
             return 'неверный пароль'
-    #а если не находим в базе пользователя
+    # а если не находим в базе пользователя
     return 'пользователь не найден'
